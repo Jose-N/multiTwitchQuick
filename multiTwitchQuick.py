2891ba83d351415ec193b825b1333b664e9e577a
Jose-N <jose.c.naylor@gmail.com>
2015-10-25 07:58:50 -0400

    Started to wokr on ability to see who is live, needs more work(reseacrch about JSON)

--- multiTwitchQuick.py
+++ multiTwitchQuick.py
@@ -2,7 +2,8 @@
 # mulitTwitchQuick.py
 # Launches a kadgar.net/live link using the Twitch usernames entered into the command line
 
-import webbrowser,sys
+import webbrowser,sys,requests
+from bs4 import BeautifulSoup
 
 # Grab usernames from command line
 if len(sys.argv) > 1:
@@ -17,4 +18,19 @@ if len(sys.argv) > 1:
 			kadgar = kadgar + streams[i] + "/"
 		webbrowser.open(kadgar)
 
-#TODO: Add ability to see who is live and for how long they have been live
+# TODO: Add ability to see who is live and for how long they have been live
+#### This might require some working with Java Script, need to reasearch ####
+#if len(sys.argv) > 1:
+	#if  "live?" == ''.join(sys.argv[1]):
+		# Grab list of channels I am following
+		#following  = requests.get('http://www.twitch.tv/dark_fleet/profile/following')
+		#following.raise_for_status()
+		# Convert HTML to BeautifulSoup object
+		#fText = BeautifulSoup(following.text, "html.parser")
+		# Parse object for names of channels I am following
+		#fList = fText.findAll("div", {"class" : "user item"})
+		#users = fList.findAll("img")
+		#print(str(len(fList)))
+		#for i in range(len(fList)):
+			#test = fText.select('img')[i]
+			#print(test)
