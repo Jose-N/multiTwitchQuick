9313674c8b6cf85e39a0271ed590df537a44eb82
Jose-N <jose.c.naylor@gmail.com>
2015-10-25 16:34:58 -0400

    Intial Commit for grabbing live list, started logic

--- multiTwitchQuick.py
+++ multiTwitchQuick.py
@@ -2,7 +2,7 @@
 # mulitTwitchQuick.py
 # Launches a kadgar.net/live link using the Twitch usernames entered into the command line
 
-import webbrowser,sys,requests
+import webbrowser,sys,requests, json
 from bs4 import BeautifulSoup
 
 # Grab usernames from command line
@@ -18,17 +18,6 @@ if len(sys.argv) > 1:
 
 # TODO: Add ability to see who is live and for how long they have been live
 #### This might require some working with Java Script, need to reasearch ####
-#if len(sys.argv) > 1:
-	#if  "live?" == ''.join(sys.argv[1]):
-		# Grab list of channels I am following
-		#following  = requests.get('http://www.twitch.tv/dark_fleet/profile/following')
-		#following.raise_for_status()
-		# Convert HTML to BeautifulSoup object
-		#fText = BeautifulSoup(following.text, "html.parser")
-		# Parse object for names of channels I am following
-		#fList = fText.findAll("div", {"class" : "user item"})
-		#users = fList.findAll("img")
-		#print(str(len(fList)))
-		#for i in range(len(fList)):
-			#test = fText.select('img')[i]
-			#print(test)
+if len(sys.argv) > 1:
+	if  "live?" == ''.join(sys.argv[1]):
+
