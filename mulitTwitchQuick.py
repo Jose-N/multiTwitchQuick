22457886d8aa8069182634fa06af450b321166b8
Jose-N <jose.c.naylor@gmail.com>
2015-10-25 04:38:14 -0400

    Outlined what i want code to do

--- mulitTwitchQuick.py
+++ mulitTwitchQuick.py
@@ -1,3 +1,11 @@
 #! python3
 # mulitTwitchQuick.py
 # Launches a kargar.net/live link using the Twitch usernames entered into the command line
+
+import browser,sys
+
+# TODO: Grab usernames from command line
+
+# TODO: Add ability to save usernames, and shorthand usernames
+
+# TODO: Add ability to add usernames 
